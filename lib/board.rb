daf28f8891cec39836d2e930839335cd0e613d43
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-10-30 05:39:15 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -7,11 +7,19 @@ class Board
     @board = Array.new(9, " ")
   end
   
-  def self.reset!
+  def reset!
     @@cells.clear
   end
   
-  def self.display
-    
+  def display
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "---------"
+    puts "#{@board[2]} | #{@board[4]} | #{@board[5]}"
+    puts "---------"
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
+  
+  def position(input)
+    input.between?(0, 8)
   end
 end
